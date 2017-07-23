fe7b9efad62c702146f36ffb3004cee15f57f898
lijing <njlijing1024@163.com>
2017-07-24 01:16:19 +0800

    增加调试参数
    Signed-off-by: lijing <njlijing1024@163.com>

--- src/main/java/otocloud/service/container/App.java
+++ src/main/java/otocloud/service/container/App.java
@@ -9,14 +9,15 @@ public class App
 {
     public static void main( String[] args )
     {  	
-/*    	try{
-    		System.out.println("enter key:");
-    		int c;
-    		c = System.in.read();
-    	}catch(Exception ex){
-    		
-    	}    	*/
-
+    	if(args != null && args.length > 0 && args[0].equals("blk")){
+    		try{
+	    		System.out.println("enter key:");
+	    		int c;
+	    		c = System.in.read();
+	    	}catch(Exception ex){
+	    		
+	    	}    		
+    	}
     	
         //System.setProperty("jaxp.debug","TRUE");
         
